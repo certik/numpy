e77eb0f62c7afa1320a4d2f1556cc150006fcc6a
Ondřej Čertík <ondrej.certik@gmail.com>
2012-08-30 19:52:48 -0700

    BUG: Add a test for Ticket #2189

--- numpy/core/tests/test_regression.py
+++ numpy/core/tests/test_regression.py
@@ -1728,5 +1728,11 @@ class TestRegression(TestCase):
         a = np.array(['abc'], dtype=np.unicode)[0]
         del a
 
+    def test_searchsorted_wrong_dtype(self):
+        # Ticket #2189, it used to segfault, so we check that it raises the
+        # proper exception.
+        a = np.array([('a', 1)], dtype='S1, int')
+        assert_raises(TypeError, np.searchsorted, a, 1.2)
+
 if __name__ == "__main__":
     run_module_suite()
